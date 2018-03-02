4ff5c64c19a68848d0a37531677444db42ccd03f
James Meyers <james.meyers@emberex.com>
2018-03-02 01:23:09 -0800

    Add static state class to View class.

--- src/View.js
+++ src/View.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import { view } from 'react-easy-state';
 
-export default view(React.Component);
+export default view(class extends React.Component {
+  constructor(props) {
+    super(props);
+    const { State } = this.constructor;
+    if(State) {
+      this.state = new State(props);
+    }
+  }
+});
