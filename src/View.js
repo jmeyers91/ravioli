99fdd6eab7056861d8d5ce8bb5cca202535ae3c8
James Meyers <james.meyers@emberex.com>
2018-03-02 08:45:43 -0800

    Remove static State class.

--- src/View.js
+++ src/View.js
@@ -1,12 +1,4 @@
 import React from 'react';
 import { view } from 'react-easy-state';
 
-export default view(class extends React.Component {
-  constructor(props) {
-    super(props);
-    const { State } = this.constructor;
-    if(State) {
-      this.state = new State(props);
-    }
-  }
-});
+export default view(React.Component);
